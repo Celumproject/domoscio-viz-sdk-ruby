4f44151ac7cc3253aa5c32eb65ee14808d1a3cc9
fthiounn <francois.thiounn@domoscio.com>
2020-10-14 17:55:11 +0200

    Error Handling : socketstimeout error on idle engine

--- lib/domoscio_viz/http_calls.rb
+++ lib/domoscio_viz/http_calls.rb
@@ -1,10 +1,13 @@
 module DomoscioViz
   module HTTPCalls
-    
     module GetUrl
       module ClassMethods
+        # In order to catch current SocketsTimeoutError that seem to appear when the VizEngine stays Idle for too long
+        # Perform the request a second time if it fails the fisrt time
         def get_url(util_name = nil, params = {})
-          DomoscioViz.request(:post, url(util_name), params)
+          response = DomoscioViz.request(:post, url(util_name), params)
+          response = DomoscioViz.request(:post, url(util_name), params) if response.is_a?(Hash) && response['success'] == false
+          response
         end
       end
 
@@ -12,6 +15,5 @@ module DomoscioViz
         base.extend(ClassMethods)
       end      
     end
-    
   end
 end
--- lib/domoscio_viz/version.rb
+++ lib/domoscio_viz/version.rb
@@ -1,3 +1,3 @@
 module DomoscioViz
-  VERSION = "0.1.1.3"
+  VERSION = "0.1.1.4"
 end
